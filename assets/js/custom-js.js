14a297eddc4bb3cd93e1bf230595a9023c7e4dc6
S M Kaiser Ahmed <kaiser331042@gmail.com>
2023-02-06 18:01:30 +0600

    added new banner

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -4,10 +4,17 @@ const popUpBanner = document.getElementById("pop-up-banner");
 const crossBtn2 = document.getElementById("cross-btn-2");
 const stickyBanner = document.getElementById("sticky-banner");
 
+const crossBtn3 = document.getElementById("cross-btn-3");
+const stickyBannerBottom = document.getElementById("sticky-banner-bottom");
+
 crossBtn.addEventListener("click", (e) => {
     popUpBanner.style.display = "none";
 })
 
 crossBtn2.addEventListener("click", (e) => {
     stickyBanner.style.display = "none";
+})
+
+crossBtn3.addEventListener("click", (e) => {
+    stickyBannerBottom.style.display = "none";
 })
