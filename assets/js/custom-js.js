2d1811442576063ce425f60bd3fc5db5fad4072f
S M Kaiser Ahmed <kaiser331042@gmail.com>
2023-02-06 14:36:02 +0600

    added new banner

---
+++ assets/js/custom-js.js
@@ -0,0 +1,6 @@
+const crossBtn = document.getElementById("cross-btn");
+const popUpBanner = document.getElementById("pop-up-banner");
+
+crossBtn.addEventListener("click", (e) => {
+    popUpBanner.style.display = "none";
+})
